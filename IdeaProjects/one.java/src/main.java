6a2a6fad0d5e97fade084f13bfe3ea5e71c9940f
r=cos(4θ) <olvadis2004@gmail.com>
2023-06-03 14:18:56 -0500

    Primer commit de Java Projects

---
+++ IdeaProjects/one.java/src/main.java
@@ -0,0 +1,14 @@
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class main {
+
+    public static void main(String[] args){
+
+
+        System.out.printf("Formatted values|%5d|%8.3f|%5s|%n",
+                          2016,Math.PI,"Hi");
+
+    }
+}
